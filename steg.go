de9604ec94351cd25ec9660d7701507d4def786e
A4BHI <manthukafc@gmail.com>
2026-02-23 22:34:20 +0530

    gm

--- steg.go
+++ steg.go
@@ -3,19 +3,33 @@ package main
 import (
 	"fmt"
 	"image"
+	_ "image/png"
 	"log"
 	"os"
 )
 
 func main() {
-	file, err := os.Open("test.png")
+	file, err := os.Open("tes.png")
 	if err != nil {
 		log.Fatal(err)
 	}
-	img, _, _ := image.Decode(file)
-	rgba, _ := img.(*image.RGBA)
+	defer file.Close()
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bounds := img.Bounds()
+	rgba := image.NewRGBA(bounds)
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			rgba.Set(x, y, img.At(x, y))
+		}
+	}
 
 	pixels := rgba.Pix
 	fmt.Println(pixels)
-
 }
+
