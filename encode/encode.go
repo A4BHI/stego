05f7b30477d2e9be3834ee9d1e3def59ba6d373f
A4BHI <manthukafc@gmail.com>
2026-02-25 22:52:14 +0530

    hm

---
+++ encode/encode.go
@@ -0,0 +1,58 @@
+package encode
+
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"log"
+	"os"
+)
+
+func Encode(targetfile string) {
+	file, err := os.Open(targetfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bounds := img.Bounds()
+	rgba := image.NewRGBA(bounds)
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			rgba.Set(x, y, img.At(x, y))
+		}
+	}
+
+	pixels := rgba.Pix
+
+	data, err := os.ReadFile("test.txt")
+	if err != nil {
+		log.Fatal("Error Reading Data: ", err)
+	}
+	index := 0
+
+	fmt.Println(data)
+	for i := 0; i < len(data); i++ {
+		for j := 7; j >= 0; j-- {
+
+			bit := (data[i] >> j) & 1
+			pixels[index] = (pixels[index] & 254) | bit
+			index++
+		}
+
+	}
+	OutFile, err := os.Create("Output.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = png.Encode(OutFile, rgba)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- steg.go
+++ steg.go
@@ -1,68 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"image"
-	"image/png"
 	_ "image/png"
-	"log"
-	"os"
+	"steg/encode"
 )
 
 func main() {
-	file, err := os.Open("tes.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	img, _, err := image.Decode(file)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	bounds := img.Bounds()
-	rgba := image.NewRGBA(bounds)
-
-	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			rgba.Set(x, y, img.At(x, y))
-		}
-	}
-
-	pixels := rgba.Pix
-
-	data, err := os.ReadFile("test.txt")
-	if err != nil {
-		log.Fatal("Error Reading Data: ", err)
-	}
-	index := 0
-
-	fmt.Println(data)
-	for i := 0; i < len(data); i++ {
-		for j := 7; j >= 0; j-- {
-
-			bit := (data[i] >> j) & 1
-			pixels[index] = (pixels[index] & 254) | bit
-			index++
-		}
-
-	}
-	OutFile, err := os.Create("Output.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = png.Encode(OutFile, rgba)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// for i := 0; i < len(pixels); i++ {
-	// 	fmt.Println(i, ":", pixels[i], "\n")
-	// }
-
-	//fmt.Println(pixels[6289403])
-
-	//fmt.Println(pixels)
-	//bitwise niggaaaa
+	encode.Encode("tes.png")
 }
