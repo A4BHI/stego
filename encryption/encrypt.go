44a9221a826bdc0deb44af3713a8ac2aee8d0106
A4BHI <manthukafc@gmail.com>
2026-03-07 23:55:35 +0530

    hmm

--- encryption/encrypt.go
+++ encryption/encrypt.go
@@ -1,5 +1,35 @@
 package encryption
 
-func Encrypt(compressedData []byte) []byte {
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"io"
+	"log"
+)
+
+func Encrypt(compressedData []byte, password string) []byte {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		log.Fatal(err)
+	}
+
+	ciphertext := gcm.Seal(nil, nonce, compressedData, nil)
+
+	return ciphertext
+
+}
+
+func generatekey(password string) {
 
 }
