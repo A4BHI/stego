2c5b83fd899e3e59cadc51ced80a8607e2660147
A4BHI <manthukafc@gmail.com>
2026-03-13 23:25:00 +0530

    hmm

--- encryption/encrypt.go
+++ encryption/encrypt.go
@@ -3,12 +3,23 @@ package encryption
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/pbkdf2"
 	"crypto/rand"
+	"crypto/sha256"
+	"fmt"
 	"io"
 	"log"
 )
 
-func Encrypt(compressedData []byte, password string) []byte {
+func Encrypt(compressedData []byte, password string) (ciphertext []byte, nonce []byte, salt []byte) {
+
+	salt = make([]byte, 16)
+
+	key, err := pbkdf2.Key(sha256.New, password, salt, 10000, 32)
+	if err != nil {
+		fmt.Println(err)
+
+	}
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		log.Fatal(err)
@@ -19,17 +30,13 @@ func Encrypt(compressedData []byte, password string) []byte {
 		log.Fatal(err)
 	}
 
-	nonce := make([]byte, gcm.NonceSize())
+	nonce = make([]byte, gcm.NonceSize())
 	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
 		log.Fatal(err)
 	}
 
-	ciphertext := gcm.Seal(nil, nonce, compressedData, nil)
+	ciphertext = gcm.Seal(nil, nonce, compressedData, nil)
 
-	return ciphertext
-
-}
-
-func generatekey(password string) {
+	return ciphertext, nonce, salt
 
 }
