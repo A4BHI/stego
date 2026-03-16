878f09ca0556aeb8915a6dd3327b4866f586c81f
A4BHI <manthukafc@gmail.com>
2026-03-16 23:03:52 +0530

    hmm

---
+++ ui/model.go
@@ -0,0 +1,14 @@
+package ui
+
+import "charm.land/lipgloss/v2"
+
+type styles struct {
+	title        lipgloss.Style
+	item         lipgloss.Style
+	selectedItem lipgloss.Style
+	help         lipgloss.Style
+	quitText     lipgloss.Style
+}
+
+type Model struct {
+}
