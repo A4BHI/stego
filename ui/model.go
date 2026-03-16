7bd44676376315299499fba0d0654aa963ada7c6
A4BHI <manthukafc@gmail.com>
2026-03-16 23:46:04 +0530

    hm

--- ui/model.go
+++ ui/model.go
@@ -4,6 +4,26 @@ import (
 	"github.com/charmbracelet/bubbles/list"
 )
 
+type item string
+
+func (i item) FilterValue() string { return string(i) }
+
 type Model struct {
 	list list.Model
+}
+
+func InitialModel() Model {
+	title := list.DefaultStyles().Title
+	items := []list.Item{
+		item("Encode"),
+		item("Decode"),
+		item("Exit"),
+	}
+
+	m := Model{
+		list: list.New(items, list.DefaultDelegate{}, 0, 0),
+	}
+
+	m.list.Title = "Choose an option from the list."
+
 }
