25d2f64d3a8af04997a3f468fde810b8fda95adb
A4BHI <manthukafc@gmail.com>
2026-03-16 23:18:40 +0530

    hmm

--- ui/model.go
+++ ui/model.go
@@ -1,14 +1,9 @@
 package ui
 
-import "charm.land/lipgloss/v2"
-
-type styles struct {
-	title        lipgloss.Style
-	item         lipgloss.Style
-	selectedItem lipgloss.Style
-	help         lipgloss.Style
-	quitText     lipgloss.Style
-}
+import (
+	"github.com/charmbracelet/bubbles/list"
+)
 
 type Model struct {
+	list list.Model
 }
