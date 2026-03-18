6e3c9941560683701d22600dcdf896b7a681f2fb
A4BHI <manthukafc@gmail.com>
2026-03-18 17:58:48 +0530

    hmm

--- ui/menu.go
+++ ui/menu.go
@@ -1,1 +1,37 @@
 package ui
+
+import (
+	"charm.land/bubbles/v2/list"
+	"charm.land/lipgloss/v2"
+)
+
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
+
+type item struct {
+	title       string
+	description string
+}
+
+func (i item) Title() string       { return i.title }
+func (i item) Description() string { return i.description }
+func (i item) FilterValue() string { return i.title }
+
+func InitialModel() Model {
+	title := list.DefaultStyles(true).Title
+
+	items := []list.Item{
+		item{"Encode", "Hide data inside an image"},
+		item{"Decode", "Extract hidden data"},
+		item{"Exit", "Quit the program"},
+	}
+
+	m := Model{
+		list:    list.New(items, list.NewDefaultDelegate(), 0, 70),
+		Welcome: "STEGO-a stegnography tool in golang",
+	}
+	m.list.Styles.Title = title
+
+	m.list.Title = "Choose an option from the list."
+
+	return m
+}
--- ui/model.go
+++ ui/model.go
@@ -1,46 +1,32 @@
 package ui
 
 import (
-	"charm.land/bubbles/v2/list"
 	tea "charm.land/bubbletea/v2"
-	"charm.land/lipgloss/v2"
 )
 
-var docStyle = lipgloss.NewStyle().Margin(1, 2)
-
-type item struct {
-	title       string
-	description string
-}
-
-func (i item) Title() string       { return i.title }
-func (i item) Description() string { return i.description }
-func (i item) FilterValue() string { return i.title }
-
 type Model struct {
-	Welcome string
-	list    list.Model
+	screen string
 }
 
-func InitialModel() Model {
-	title := list.DefaultStyles(true).Title
+// func InitialModel() Model {
+// 	title := list.DefaultStyles(true).Title
 
-	items := []list.Item{
-		item{"Encode", "Hide data inside an image"},
-		item{"Decode", "Extract hidden data"},
-		item{"Exit", "Quit the program"},
-	}
+// 	items := []list.Item{
+// 		item{"Encode", "Hide data inside an image"},
+// 		item{"Decode", "Extract hidden data"},
+// 		item{"Exit", "Quit the program"},
+// 	}
 
-	m := Model{
-		list:    list.New(items, list.NewDefaultDelegate(), 0, 70),
-		Welcome: "STEGO-a stegnography tool in golang",
-	}
-	m.list.Styles.Title = title
+// 	m := Model{
+// 		list:    list.New(items, list.NewDefaultDelegate(), 0, 70),
+// 		Welcome: "STEGO-a stegnography tool in golang",
+// 	}
+// 	m.list.Styles.Title = title
 
-	m.list.Title = "Choose an option from the list."
+// 	m.list.Title = "Choose an option from the list."
 
-	return m
-}
+// 	return m
+// }
 
 func (m Model) Init() tea.Cmd {
 	return nil
